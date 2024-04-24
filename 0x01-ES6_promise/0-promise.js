86b91ba426565408fbbb966af871e4d60aa43611
melchizedek2991 <ayodeleayomidotun@gmail.com>
2024-04-24 06:23:01 -0700

    promises frontend alx

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,11 +1,3 @@
-export const getResponseFromAPI = () => {
-    const success = true;
-    return new Promise((resolve, reject) => {
-        if (success) {
-            resolve("Response from API");
-        } else {
-            reject("Error fetching data from API");
-        }
-    });
-};
-
+export default function getResponseFromAPI() {
+  return (new Promise((resolve, reject) => {}));
+}
