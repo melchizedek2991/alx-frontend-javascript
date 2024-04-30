b0d980422b714be80e094b563742b2c6a10c554a
melchizedek2991 <ayodeleayomidotun@gmail.com>
2024-04-30 11:45:37 -0700

    data

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,8 +1,7 @@
-const createInt8TypedArray = (length, position, value) => {
-        const buffer = new ArrayBuffer(length);
-        const intArr = new IntArray(buffer, 0);
-        if (position > length - 1) throw Error('Position outside range');
-
-        intArr.setInt8(position, value);
-        return intArr;
+export default function createInt8TypedArray(length, position, value) {
+  const buff = new ArrayBuffer(length);
+  const view = new DataView(buff, 0);
+  if (position > length - 1) throw Error('Position outside range');
+  view.setInt8(position, value);
+  return view;
 }
