bd0bf9401c7b5adb33cb487888129337cad2964b
melchizedek2991 <ayodeleayomidotun@gmail.com>
2024-04-30 11:57:09 -0700

    ES6 data manipulation

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,9 +1,20 @@
-export default function updateStudentGradeByCity(std, cty, newGrade){
-  return std
-    .filter((stud) => stud.location == cty)
+export default function updateStudentGradeByCity(stds, city, newGrade){
+  return stds
+    .filter((stud) => stud.location == city)
     .map((stud) => {
       let grades = newGrade.find((grade) => grade.studId == stud.id);
       let grade = grades ? grades.grade : "N/A";
       return { ...stud, grade };
      });
 }
+
+/* eslint-disable
+export default function updateStudentGradeByCity(students, city, newGrade){
+  return students
+    .filter((student) => student.location == city)
+    .map((student) => {
+      let grades = newGrade.find((grade) => grade.studentId == student.id);
+      let grade = grades ? grades.grade : "N/A";
+      return { ...student, grade };
+     });
+}*/
